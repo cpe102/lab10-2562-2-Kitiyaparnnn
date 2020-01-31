c2db629245e42a0df6af81e910efb099fab3c99b
lab314-14@labeng.local <lab314-14@labeng.local>
2020-01-31 15:02:11 +0700

    final

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,39 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+    int x;
+    string g;
+    cout<<"Press Enter 3 times to reveal your future.";
+    for(int i=0;i<3;i++) cin.get();
+    srand(time(0));
+    x=rand() % 9 + 1;
+    switch(x){
+        case 1: g="A";
+        break;
+        case 2: g="B+";
+        break;
+        case 3: g="B";
+        break;
+        case 4: g="C+";
+        break;
+        case 5: g="C";
+        break;
+        case 6: g="D+";
+        break;
+        case 7: g="D";
+        break;
+        case 8: g="F";
+        break;
+        case 9: g="W";
+        break;
+    }
+    cout<<"You will get "<<g<<" in this 261102.";
+}
+
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
